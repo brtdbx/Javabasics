8309e446b1b00808905ad0ba809d644256328a42
br <>
2017-05-09 06:18:56 +0200

    go

--- src/test/java/brUtils/MathUtilsTest.java
+++ src/test/java/brUtils/MathUtilsTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package brUtils;
 
 import java.math.BigDecimal;
@@ -13,10 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author 1790
- */
 public class MathUtilsTest {
     
     public MathUtilsTest() {
--- src/test/java/com/mycompany/javabasics/AppTest.java
+++ src/test/java/com/mycompany/javabasics/AppTest.java
@@ -5,29 +5,27 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
-    
+
     public AppTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-
 
     /**
      * Test of dateTimeTests method, of class App.
@@ -38,45 +36,8 @@ public class AppTest {
         App instance = new App();
         instance.dateTimeTests();
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("hallo from tests");
+        //fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of showTextProps method, of class App.
-     */
-    @Test
-    public void testShowTextProps() {
-        System.out.println("showTextProps");
-        App instance = new App();
-        instance.showTextProps();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of printUtf8CharTable method, of class App.
-     */
-    @Test
-    public void testPrintUtf8CharTable() throws Exception {
-        System.out.println("printUtf8CharTable");
-        App instance = new App();
-        instance.printUtf8CharTable();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of splitTest method, of class App.
-     */
-    @Test
-    public void testSplitTest() {
-        System.out.println("splitTest");
-        App instance = new App();
-        instance.splitTest();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-
-    
 }
